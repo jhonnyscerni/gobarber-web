3755b543d038e998d4c2abad07643fcb159402a8
jhonnyscerni <jhonnyscerni@gmail.com>
2021-09-15 17:50:52 -0300

    #02 - Configurando Css Global

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global'
+
 const App: React.FC = () => (
-    <h1>Hello world</h1>
+    <>
+        <h1>Hello world</h1>
+        <GlobalStyle/>
+    </>
+
 );
 
 export default App;
